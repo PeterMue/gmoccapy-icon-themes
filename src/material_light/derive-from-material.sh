3016328281b7ea0178f0908e6f43dc752d93647c
Peter Müller <3819198+PeterMue@users.noreply.github.com>
2022-03-11 12:35:32 +0100

    Add script to derive icons from material
    * derive-from-material.sh: Derive icons, index.theme and build.py from material theme modifying the colors and theme name.
    * build.py: derived from material
    * main_switch_icons.inkscape.svg: Derive from material AND modify arrow color.

---
+++ src/material_light/derive-from-material.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+THEME_NAME="material-light"
+THEME_COMMENT="Material icon theme for gmoccapy (optimized for light themes)"
+
+BASE_COLOR="#212121"
+SUCCESS_COLOR="#00C853"
+WARN_COLOR="#FFD600"
+ERROR_COLOR="#DD2C00"
+
+pushd "$(dirname "$(realpath "$0")")"
+
+# derive build script
+sed -e "s/\"material\"/\"$THEME_NAME\"/g" -e "s/\.symbolic\.png/\.png/g" ../material/build.py > build.py
+
+# derive index.theme
+sed -e "s/^Name=\(.*\)$/Name=$THEME_NAME/g" -e "s/^Comment=\(.*\)$/Comment=$THEME_COMMENT/g" ../material/index.theme > index.theme
+
+# derive SVGs
+for svg in ../material/svg/*.svg; do
+  FILENAME=$(basename $svg)
+  sed -e "s/#000000/$BASE_COLOR/g" -e "s/#ff0000/$SUCCESS_COLOR/g" -e "s/#00ff00/$WARN_COLOR/g" -e "s/#0000ff/$ERROR_COLOR/g" $svg > "svg/$FILENAME"
+done
+
+popd
